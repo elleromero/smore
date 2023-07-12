9ab37d36ee0fcb4ea8c8eee70e1c6af4a35dd472
omineko <121761913+omineko@users.noreply.github.com>
2023-07-12 22:56:15 +0800

    initial commit

---
+++ resources/js/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div id="app">
+            <h1>This is thje root component</h1>
+            <h1>wsdkgsdkj</h1>
+        </div>
+    );
+}
