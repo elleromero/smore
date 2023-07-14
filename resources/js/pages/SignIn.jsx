43d8b2c7d2e61c81859a3a89ca77237711248fc7
omineko <121761913+omineko@users.noreply.github.com>
2023-07-14 17:49:20 +0800

    added assets

--- resources/js/pages/SignIn.jsx
+++ resources/js/pages/SignIn.jsx
@@ -4,15 +4,16 @@ import { EyeOff, Key, Mail } from "react-feather";
 
 export default function SignIn() {
     return (
-        <main className="container mx-auto flex justify-between">
-            <section className="left">
+        <main className="container mx-auto grid grid-cols-3 gap-x-9">
+            <section className="left self-center">
                 <h1 className="header-1">Sign In</h1>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-                    ipsum ex mollitia, quidem repellendus dolorem velit eveniet
-                    exercitationem odio numquam.
+                    Ignite Your Project Management Success! Seamlessly
+                    orchestrate your projects with Smore, the ultimate platform
+                    designed to simplify collaboration, streamline tasks, and
+                    empower your team's productivity.
                 </p>
-                <p>
+                <p className="mt-3">
                     If you don't have an account you can &nbsp;
                     <a href="#" className="text-primary font-semibold">
                         Register here
@@ -20,9 +21,11 @@ export default function SignIn() {
                 </p>
             </section>
 
-            {/* image */}
+            <div>
+                <img src="/assets/svg/gummy-work-from-home.svg" alt="image" />
+            </div>
 
-            <form action="" method="">
+            <form action="" method="" className="self-center">
                 <InputComponent
                     type="text"
                     placeholder="Email"
