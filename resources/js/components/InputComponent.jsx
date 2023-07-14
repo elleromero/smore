486852a108a3b6fb04ba0509fc0ca5fcb949e0f4
omineko <121761913+omineko@users.noreply.github.com>
2023-07-14 17:18:47 +0800

    added input component

---
+++ resources/js/components/InputComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function InputComponent({ icon: Icon, ...props }) {
+    return (
+        <div className="bg-slate-200 relative p-3 my-5">
+            <Icon className="absolute bottom-1/2 right-3 translate-y-1/2 text-gray-500" />
+            <input
+                {...props}
+                className="bg-transparent outline-none w-full pr-8"
+            />
+        </div>
+    );
+}
--- resources/js/pages/SignIn.jsx
+++ resources/js/pages/SignIn.jsx
@@ -1,31 +1,45 @@
 import React from "react";
+import InputComponent from "../components/InputComponent";
+import { EyeOff, Key, Mail } from "react-feather";
 
 export default function SignIn() {
     return (
-        <main className="container mx-auto">
-            <h1>Sign In</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-                ipsum ex mollitia, quidem repellendus dolorem velit eveniet
-                exercitationem odio numquam.
-            </p>
-            <p>
-                If you don't have an account you can &nbsp;
-                <a href="#" className="text-primary font-semibold">
-                    Register here
-                </a>
-            </p>
+        <main className="container mx-auto flex justify-between">
+            <section className="left">
+                <h1>Sign In</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+                    ipsum ex mollitia, quidem repellendus dolorem velit eveniet
+                    exercitationem odio numquam.
+                </p>
+                <p>
+                    If you don't have an account you can &nbsp;
+                    <a href="#" className="text-primary font-semibold">
+                        Register here
+                    </a>
+                </p>
+            </section>
 
             {/* image */}
 
             <form action="" method="">
-                <div className="email">
-                    <input type="email" name="" id="" />
-                </div>
-                <div className="password">
-                    <input type="password" name="" id="" />
-                </div>
-                <button type="submit" className="btn btn-primary">
+                <InputComponent
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    icon={Mail}
+                />
+
+                <InputComponent
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    icon={Key}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-primary shadow-primary shadow-2xl w-full"
+                >
                     Sign In
                 </button>
             </form>
