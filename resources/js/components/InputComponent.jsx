d0b7af02a23bfcf74adcf33323406bafe2775e1d
omineko <121761913+omineko@users.noreply.github.com>
2023-07-15 21:12:49 +0800

    added checker for null icon

--- resources/js/components/InputComponent.jsx
+++ resources/js/components/InputComponent.jsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function InputComponent({ icon: Icon, ...props }) {
     return (
         <div className="bg-slate-200 relative p-3 my-5 rounded-md">
-            <Icon className="absolute bottom-1/2 right-3 translate-y-1/2 text-gray-500" />
+            {Icon && (
+                <Icon className="absolute bottom-1/2 right-3 translate-y-1/2 text-gray-500" />
+            )}
             <input
                 {...props}
-                className="bg-transparent outline-none w-full pr-8"
+                className={`bg-transparent outline-none w-full ${
+                    Icon && "pr-8"
+                }`}
             />
         </div>
     );
