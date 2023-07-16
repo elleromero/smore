749271b7f87c01fd328b77a3bb133d74e0030093
omineko <121761913+omineko@users.noreply.github.com>
2023-07-16 20:55:19 +0800

    added error

--- resources/js/components/InputComponent.jsx
+++ resources/js/components/InputComponent.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
-export default function InputComponent({ icon: Icon, ...props }) {
+export default function InputComponent({ icon: Icon, error, ...props }) {
     return (
-        <div className="bg-slate-200 relative p-3 my-5 rounded-md">
-            {Icon && (
-                <Icon className="absolute bottom-1/2 right-3 translate-y-1/2 text-gray-500" />
-            )}
-            <input
-                {...props}
-                className={`bg-transparent outline-none w-full ${
-                    Icon && "pr-8"
+        <div className="my-4">
+            <div
+                className={`bg-slate-200 relative p-3 rounded-md ${
+                    error && "border-red-400 border-2"
                 }`}
-            />
+            >
+                {Icon && (
+                    <Icon className="absolute bottom-1/2 right-3 translate-y-1/2 text-gray-500" />
+                )}
+                <input
+                    {...props}
+                    className={`bg-transparent outline-none w-full ${
+                        Icon && "pr-8"
+                    }`}
+                />
+            </div>
+            {error && (
+                <span className="text-red-400 font-semibold mt-2 inline-block">
+                    {error}
+                </span>
+            )}
         </div>
     );
 }
